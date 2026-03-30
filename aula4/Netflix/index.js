8020325ed923f5cbc4d896da0dc9bff0f3f7a4dc
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2026-03-30 18:10:35 -0300

    Add files via upload

---
+++ aula4/Netflix/index.js
@@ -0,0 +1,2 @@
+alert("Bem vnido ao Netflix! Clique em um perfil para acessar o catálogo de filmes e séries.");
+console.log("Bem vnido ao Netflix! Clique em um perfil para acessar o catálogo de filmes e séries.");
